93a10e7c77978ed98c2bc47026ec0dc51b67ae09
hamzaclini <155651110+hamzaclini@users.noreply.github.com>
2024-06-27 16:20:21 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,190 +1,48 @@
-const timerElement = document.getElementById('timer');
-const label = document.getElementById("output");
-const say = document.getElementById("say");
-const startBtn = document.getElementById("start");
-const stopBtn = document.getElementById("stop");
-const box = document.getElementById("textbox");
+let playerState = 'idle';
+let stateIndex = 0;
+const dropdown = document.getElementById('animations');
+let states = ['idle', 'jump', 'fall', 'run', 'dizzy', 'sit', 'roll', 'bit', 'ko', 'gethit'] 
+dropdown.addEventListener('change', function(e){
+    playerState = e.target.value;
+    stateIndex = states.indexOf(playerState);
+})
+const canvas = document.getElementById('canvas1')
+const ctx = canvas.getContext('2d');
 
-let letter;
-let correctWords = [];
-let wrongWords = [];
-let repeatedWords = [];
-let mediaRecorder;
-let socket;
-let working = false;
-let consider;
-let already;
-let timerInterval;
 
-function check_word() {
-  for (let i = 0; i < correctWords.length; i++) {
-    if (correctWords[i] === consider) {
-      return true;
+const CANVAS_WIDTH = canvas.width = 600;
+const CANVAS_HEIGHT = canvas.height = 600;
+
+const playerImage = new Image();
+playerImage.src = 'shadow_dog.png';
+const spriteWidth = 575;
+const spriteHeight = 523;
+let frameX = 0;
+//let frameY = 0;
+let maxFrameY = [6, 6, 6, 8, 10, 4, 6, 6, 11, 3];
+
+
+console.log(playerState);
+let gameFrame = 3; // control the speed of frames
+let countFrame = 0;
+
+
+function animate(){
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    //ctx.fillRect(50, 50, 100, 100);
+    //ctx.drawImage(playerImage, sx, sy, sw, sh, 50, 50, CANVAS_WIDTH, CANVAS_HEIGHT); s: source
+    ctx.drawImage(playerImage, frameX * spriteWidth, stateIndex * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight );
+    if(countFrame >= gameFrame){
+        if(frameX < maxFrameY[stateIndex]) frameX++;
+        else frameX = 0;
+        countFrame = 0;
+
     }
-  }
-  return false;
-}
+    countFrame++;
+    
 
-function getUniqueWords(words) {
-  const uniqueWordsSet = new Set(words);
-  const uniqueWordsArray = Array.from(uniqueWordsSet);
-  return uniqueWordsArray;
-}
+    requestAnimationFrame(animate);
+};
 
-startBtn.addEventListener("click", function () {
-  clearInterval(timerInterval);
-  timerElement.textContent = '120 seconds'
-  experimentStartTime = Date.now();
-  timerInterval = setInterval(updateTimer, 1000);
-  letter = box.value;
+animate();
 
-  if (/^[a-zA-Z]$/.test(letter)) {
-    letter = letter.toLowerCase();
-    if (working === false) {
-      box.disabled = true;
-      startBtn.innerHTML = "Stop";
-      say.textContent = "Tu as dit:";
-      start_recording();
-      working = true;
-    } else {
-      clearInterval(timerInterval);
-      console.log(correctWords);
-      repeatedWords = getUniqueWords(repeatedWords);
-      save_csv()
-      label.textContent = "";
-      say.textContent = "Merci pour avoir participé à ce test";
-      box.disabled = false;
-      working = false;
-      startBtn.innerHTML = "Re-commence";
-      stop_recording();
-    }
-  } else {
-    alert("Il faut entrer un lettre!");
-  }
-});
-
-function start_recording() {
-  navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
-    mediaRecorder = new MediaRecorder(stream, { mimeType: "audio/webm" });
-
-    socket = new WebSocket(
-      "wss://api.deepgram.com/v1/listen?language=fr&tier=enhanced&model=general&version=beta",
-      ["token", "dbe9ccbb4608e6c5e8b1baed8880d0627480af28"]
-    );
-
-    //wss://api.deepgram.com/v1/listen
-    //wss://api.deepgram.com/v1/listen?language=fr-FR
-
-    socket.onopen = () => {
-      mediaRecorder.addEventListener("dataavailable", (event) => {
-        socket.send(event.data);
-      });
-      mediaRecorder.start(250);
-    };
-
-    socket.onmessage = (message) => {
-      const received = JSON.parse(message.data);
-      const transcript = received.channel.alternatives[0].transcript;
-      //console.log(transcript);
-      consider = transcript.split(" ")[0];
-      if (consider === "") {
-        //console.log("empty");
-      } else {
-        wordLetters = consider.split("");
-        if (wordLetters[0] === letter) {
-          already = check_word();
-          console.log(already);
-          if (already) {
-            label.classList.remove("wrong");
-            label.classList.remove("right");
-            label.classList.add("repeated");
-            repeatedWords.push(consider);
-          } else {
-            correctWords.push(consider);
-            label.classList.remove("repeated");
-            label.classList.remove("wrong");
-            label.classList.add("right");
-          }
-
-          //console.log("right");
-        } else {
-          wrongWords.push(consider);
-          label.classList.remove("repeated");
-          label.classList.remove("right");
-          label.classList.add("wrong");
-          //console.log("wrong");
-        }
-        label.textContent = "\t" + consider;
-        //console.log(wordLetters);
-      }
-    };
-
-    socket.onerror = (error) => {
-      //console.error("WebSocket error:", error);
-    };
-  });
-}
-
-function stop_recording() {
-  if (mediaRecorder && mediaRecorder.state !== "inactive") {
-    mediaRecorder.stop();
-  }
-  // if (
-  //   socket &&
-  //   (socket.readyState === WebSocket.OPEN ||
-  //     socket.readyState === WebSocket.CONNECTING)
-  // ) {
-  //   socket.close();
-  // }
-}
-
-function updateTimer() {
-  const currentTime = Date.now();
-  elapsedSeconds = Math.floor((currentTime - experimentStartTime) / 1000);
-  let remainingSeconds = 120 - elapsedSeconds;
-  
-  if (remainingSeconds <= 0) {
-      clearInterval(timerInterval); 
-      if(remainingSeconds === 0){
-      console.log(correctWords);
-      repeatedWords = getUniqueWords(repeatedWords);
-      save_csv()
-      label.textContent = "";
-      say.textContent = "Merci pour avoir participé à ce test";
-      box.disabled = false;
-      working = false;
-      startBtn.innerHTML = "Re-commence";
-      stop_recording();
-      }
-  }
-
-  if (remainingSeconds <= 10) {
-      timerElement.style.color = 'red'; 
-  }
-  
-  timerElement.textContent =  remainingSeconds + ' seconds';
-  console.log(remainingSeconds);
-}
-
-function save_csv(){
-  correctWords.unshift("Correcte");
-  repeatedWords.unshift("Doublons");
-  wrongWords.unshift("Erreur");
-  const data = [correctWords, repeatedWords, wrongWords];
-  function transpose(array) {
-    return array[0].map((_, colIndex) => array.map(row => row[colIndex]));
-  }
-  const transposedData = transpose(data);
-  const csvData = transposedData.map(row => row.join(';')).join('\n');
-  const utf8BOM = "\uFEFF";
-  const csvDataUTF8 = utf8BOM + csvData;
-  const blob = new Blob([csvDataUTF8], { type: 'text/csv;charset=utf-8' });
-  const url = URL.createObjectURL(blob);
-  const a = document.createElement('a');
-  a.href = url;
-  a.download = 'emotion_data.csv';
-  document.body.appendChild(a);
-  a.click();
-  document.body.removeChild(a);
-  //window.close(); 
-}
